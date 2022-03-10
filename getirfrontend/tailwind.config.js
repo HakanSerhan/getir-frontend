bb60d7fcad82e2a200b2b7d0c3103c50206ea632
Hakan Serhan <hakanserhanigde@gmail.com>
2022-03-11 02:36:09 +0300

    Getir Front End using React and Tailwind
    Getir Front End using React and Tailwind

---
+++ getirfrontend/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  mode: 'jit',
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'brand-color': '#4c3398',
+        'primary-brand-color': '#5d3ebc',
+        'secondary-brand-color': '#7849f7',
+        'brand-yellow': '#ffd300'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
